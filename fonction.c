736d1553d6e317cf3166318797d280e7b4f41257
phousphoure <paul.brouet@viacesi.fr>
2016-12-15 12:15:16 +0100

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -97,50 +97,46 @@ void affPBM(char* chaine)
 
 }
 
-	
 
-
-void center (char* text, Taille* taille)
+void center (char* text/*, Taille *taille*/) //fonctione permettant de centrer une chaine de caractère dans la console.
 {
-    struct winsize w;								//on appele une structure dans une librairie ainsi que des variable permettant d'avoir la taille de la console 
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int col = w.ws_col;
-    int row = w.ws_row;
+	struct winsize w; //structure de la librairie 
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); //appel système d'entrée-sortie 
+	int largeurCsl = w.ws_col;	//variable du nombre de colonnes de la console, sa largeur
+	int hauteurCsl = w.ws_row; //variable du nombre de lignes de la console, sa hauteur
+	unsigned int n = 0, i = 0, j = 0;	// Variables utilisées dans les boucles for.
 
-    int height = taille->width ;                     //variable de hauteur de l'image
-    int width = taille->height;                        //variable de largeur de l'image 
-    unsigned int i = 0;
-    unsigned int n;
-    unsigned int x;
+	int height = 5; //variable de hauteur de l'image
+	int width = 5;	//variable de largeur de l'image 
 
-    system("clear");
+	system("clear"); //vide la console
 
-    for (n = 0; n < (row - height)/2; n++)			// on rajoute des blancs entre la différences de taille de la console et de l'image le tout diviser par deux (ici en haut)
-    {
-        putchar('\n');
-    }
+	for (n = 0; n < (hauteurCsl - height)/2; n++) //boucle permettant d'ajouter les \n nécessaires au dessus de l'image en fonction de la taille de la console et de l'image
+	{
+		putchar('\n');
+	}
 
-    for (int j = 0; j < (col - width) / 2; j++)   
-    {
-        putchar(' ');
-    }
+	for (int j = 0; j < (largeurCsl - width) / 2; j++)	//boucle ajoutant les espaces nécessaires pour la premiere ligne de la chaine de caractères
+	{	
+		putchar(' ');
+	}
 
-    for (int i = 1; i < strlen(text); i++)
-    {
-        printf("%c", text[i]);
-        if(text[i] == '\n')
-        {
-            for (int j = 0; j < (col - width) / 2; j++) 
-            	{
-              	  putchar(' ');
-           		}
-        }
-    }
-    printf("\n");
-    for (n = 0; n < (row - height)/2; n++)
-    {
-        putchar('\n');
-    }
+	for (i = 0; i < strlen(text); i++) //boucle parcourant toute la chaine de caractères qui affiche tous les caractères.
+	{
+		printf("%c", text[i]);
+		if(text[i] == '\n')  //si le caractère rencontré est un \n
+		{
+			for (int j = 0; j < (largeurCsl - width) / 2; j++) //permet d'ajouter les espaces pour toutes les autres lignes 
+			{
+				putchar(' ');
+			}
+		}
+	}
+	printf("\n");
+	for (n = 0; n < (hauteurCsl - height)/2; n++) 	//permet d'ajouter les \n nécessaires en dessous en fonction de la taille de la console et de l'image
+	{										
+		putchar('\n');
+	}
 }
 
 
